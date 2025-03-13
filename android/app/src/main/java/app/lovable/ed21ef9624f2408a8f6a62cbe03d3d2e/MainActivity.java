990d5fd50e5746ba3beb462e8e507317e3fb3316
Idris <toxywipe08@gmail.com>
2025-03-13 15:42:32 +0100

    activation app

---
+++ android/app/src/main/java/app/lovable/ed21ef9624f2408a8f6a62cbe03d3d2e/MainActivity.java
@@ -0,0 +1,5 @@
+package app.lovable.ed21ef9624f2408a8f6a62cbe03d3d2e;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,19 +1,9 @@
-
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.ed21ef9624f2408a8f6a62cbe03d3d2e',
-  appName: 'lunch-logger',
-  webDir: 'dist',
-  server: {
-    url: 'https://ed21ef96-24f2-408a-8f6a-62cbe03d3d2e.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  plugins: {
-    SplashScreen: {
-      launchAutoHide: false,
-    },
-  },
+  appId: 'com.cantineoID.app',
+  appName: 'cantineoId',
+  webDir: 'dist'
 };
 
 export default config;
